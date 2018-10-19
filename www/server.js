5ec70a277c878e07b11fbcb6e65a336906107c18
James McDaniel <icathaid@gmail.com>
2018-10-18 17:06:23 -0700

    servers running, not really rendering much yet

--- www/server.js
+++ www/server.js
@@ -5,3 +5,37 @@ const express = require('express');
 
 const app = express();
 
+const PORT = process.env.PORT || 8080;
+const API = process.env.API_URL || 'http://localhost:3000';
+
+
+//  set up ejs as our template engine:
+
+app.set('view engine', 'ejs');
+
+
+//  set the public folder for static assets:
+app.use( express.static('./public') );
+
+app.get('/', homePage);
+app.get('/products/', productsView);
+// app.get('/teams/', teamsPage);
+
+
+function homePage( req, res ){
+  res.render('site', {page:'./pages/home',title:'Our Site: Home'});
+}
+
+function productsView(req, res){
+  superagent.get(`${API}/products`)
+    .then(data => {
+      console.log(data.body);
+      res.render('site', {page:'./pages/products',title:'Products page'});
+    })
+    .catch(err => {console.log('errrrrr:     ', err);
+    });
+}
+
+app.listen(PORT, () => {
+  console.log(`listening on PORT ${PORT}`);
+});
